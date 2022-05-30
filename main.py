6de329bef1eb9a437b50c0a9656bbaaceb850e3f
jyryu3161 <jyryu3161@kaist.ac.kr>
2022-05-30 09:13:08 +0900

    update

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import tqdm
 import utils
 import os
 import glob
+import subprocess
+
 warnings.filterwarnings("ignore")
 
 def calc_sim(original_feat, pred_feat):
@@ -31,10 +33,6 @@ def check_structure_original(smiles, loaded_encoder_model, fingerprint_type, thr
     sim = calc_sim(feat[0], pred_feat[0])
     return sim
 
-    if sim >= threshold:
-        return True
-    else:
-        return False
 
 def check_structure_scaffold(smiles, loaded_encoder_model, fingerprint_type, threshold):
     frags = utils.get_scaffolds(smiles)
@@ -50,17 +48,18 @@ def check_structure_scaffold(smiles, loaded_encoder_model, fingerprint_type, thr
     pred_feat = np.where(results > 0.5, 1, 0)
     
     flag = True
-    min_sim = 1.1
-    
+    min_sim = 1.0
+    sim_list = []
     for i in range(len(feat)):
         sim = calc_sim(feat[i], pred_feat[i])
+        sim_list.append(sim)
         if sim < min_sim:
             min_sim = sim
             
         #if sim < threshold:
         #    flag = False
     #return flag
-    return min_sim
+    return np.min(sim_list)
 
 def read_smiles(smiles_input):
     smiles_list = []
@@ -69,28 +68,37 @@ def read_smiles(smiles_input):
             smiles_list.append(line.strip())
     return smiles_list
 
-def predict(smiles):
-    model_json = './models/scaffold/256_1_0.001.json'
-    model_weight = './models/scaffold/256_1_0.001.h5'
+# def predict(smiles):
+#     model_json = './models/scaffold/256_1_0.001.json'
+#     model_weight = './models/scaffold/256_1_0.001.h5'
     
-    json_file = open(model_json, "r")
-    loaded_model_json = json_file.read() 
-    json_file.close()
+#     json_file = open(model_json, "r")
+#     loaded_model_json = json_file.read() 
+#     json_file.close()
     
-    loaded_encoder_model = model_from_json(loaded_model_json)
-    loaded_encoder_model.load_weights(model_weight)
+#     loaded_encoder_model = model_from_json(loaded_model_json)
+#     loaded_encoder_model.load_weights(model_weight)
     
-    flag = check_structure_scaffold(smiles, loaded_encoder_model)
-    return flag
+#     flag = check_structure_scaffold(smiles, loaded_encoder_model)
+#     return flag
+
+def run_chemprop(output_dir):
+    subprocess.call('chemprop_predict --test_path %s/fingerprint_result.csv --checkpoint_dir models/Scaffold_config_save --preds_path %s/final_result.csv --features_generator rdkit_2d_normalized --no_features_scaling --ensemble_variance'%(output_dir, output_dir), shell=True, stderr=subprocess.STDOUT)
+    return
 
 if __name__ == '__main__':
     parser = utils.argument_parser()    
     options = parser.parse_args()
     smiles_input = options.smiles_input
-    output_file = options.output_dir    
+    output_dir = options.output_dir    
     scaffold_flag = options.scaffold_flag
     threshold = 0.8
     models = {}
+    
+    try:
+        os.mkdir(output_dir)
+    except:
+        pass
     
     if not scaffold_flag:
         folders = glob.glob('./models/original_*')
@@ -125,8 +133,8 @@ if __name__ == '__main__':
     
     smiles_list = read_smiles(smiles_input)
     
-    fp = open(output_file, 'w')
-    fp.write('%s\t%s\t%s\t%s\t%s\n'%('Smiles', 'ECFP4', 'MACCS', 'Daylight', 'PubChem'))
+    fp = open(output_dir+'/fingerprint_result.csv', 'w')
+    fp.write('%s,%s,%s,%s,%s\n'%('Smiles', 'ECFP4', 'MACCS', 'Daylight', 'PubChem'))
     for smiles in tqdm.tqdm(smiles_list):
         try:
             result_info = {}
@@ -138,7 +146,11 @@ if __name__ == '__main__':
             flag_list = []
             for each_model in ['ecfp', 'maccs', 'daylight', 'pubchem']:
                 flag_list.append(str(result_info[each_model]))
+            fp.write('%s,%s\n'%(smiles, ','.join(flag_list)))
         except:
             continue
-        fp.write('%s\t%s\n'%(smiles, '\t'.join(flag_list)))    
+            
     fp.close()
+    
+    run_chemprop(output_dir)
+    
--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
-#python main.py -i ./input/ZINC_sample.txt  -o ./output/ZINC_out_scaffold.txt -s
-#python main.py -i ./input/ZINC_sample.txt  -o ./output/ZINC_out.txt
+# python main.py -i ./input/ZINC_sample.txt  -o ./output/ZINC_out_scaffold.txt -s
+# python main.py -i ./input/ZINC_sample.txt  -o ./output/ZINC_out.txt
+# CUDA_VISIBLE_DEVICES=0 python main.py -i ./input/CHEML_sample.txt  -o ./output/CHEML_out.txt
 
-python main.py -i ./input/input.txt  -o ./output/output_scaffold_test.txt -s
-python main.py -i ./input/input.txt  -o ./output/output_original_test.txt
+
+#python main.py -i ./input/test.txt  -o ./output_test -s
+#python main.py -i ./input/drugbank.txt  -o ./output_drugbank -s
+
+#python main.py -i ./input/test.txt  -o ./output_test2
+#python main.py -i ./input/drugbank.txt  -o ./output_drugbank2
+
+# python main.py -i ./input/test2.txt  -o ./output_test2_2
+
+python main.py -i ./input/uba6_smiles.txt  -o ./output_uba6
