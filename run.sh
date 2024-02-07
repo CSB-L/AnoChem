576f10c128b19eabb298d56d4c8c5f3090f061c5
Changdai Gu <cdgu@oncocross.com>
2024-02-07 15:03:57 +0900

    environment optimized

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+# For ordinary usage
 python main.py -i ./input/test.smi  -o ./output_test
-python main.py -i ./input/test.smi  -o ./output_test_calc_all -c
+
+# -c, --calc_all : To calculate all the subscores not used as an input feature for ensemble model
+# -b INT, --bit_image INT : generation of images for each input SMILES and possible revising targets for the structure.
+python main.py -i ./input/test.smi  -o ./output_test_calc_all -c -b 8
